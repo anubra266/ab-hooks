4bcd84d8788a60168c8c07c503b10dafbdd9f11b
anubra266 <anubra266@gmail.com>
2020-11-05 14:48:12 +0100

    Add index Types declaration

---
+++ types/index.d.ts
@@ -0,0 +1,5 @@
+export { useUrlQuery } from "./lib/url";
+export { useFCap, usePlural } from "./lib/string";
+export { useDualRef, useDynamicRefs, useExpose, useIprops, useTitle, useTheme, useThemeVar, useScroll, useSmoothRefresh } from "./lib/app";
+export { useAsort, useShuffle } from "./lib/array";
+export { useAntDate, useDbDate, useHumanDate } from "./lib/datez";
